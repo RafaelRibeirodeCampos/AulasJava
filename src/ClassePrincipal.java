cc808a28d7b54b9abd98e98de9e930c29c6ea2ec
RafaelRibeirodeCampos <rafael.campos77@etec.sp.gov.br>
2025-03-20 12:25:50 -0300

    Inserido arquivo Java com ClassePrincipal e System.out

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -2,8 +2,10 @@
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Imprimir mensagem
+		System.out.println("Hello, world!");//sysout & CTRL + SPACE
+		//Imprimir mensagem sem pular linha
+		System.out.printf("Olá, mundo!");
 	}
 
 }
